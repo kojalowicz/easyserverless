87f527b0e8fa2d145f8436aed7ceb656f0ad84a7
kojalowicz <36206988+kojalowicz@users.noreply.github.com>
2022-02-04 10:40:16 +0100

    Workflow andvertex ai (#2)
    * Release 2.0
    
    Co-authored-by: kojalowicz <piotr.kojalowicz@gmial.com>

--- terraform/function.tf
+++ terraform/function.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = "salesrecon"
-  region  = "europe-west1"
-}
-
 resource "google_storage_bucket" "bucket" {
   name     = "salesrecon"
   location = "europe-west1"
@@ -19,7 +14,6 @@ resource "google_cloudfunctions_function" "function" {
   project = "salesrecon"
   name        = "coca-cola-function"
   runtime     = "python39"
-
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "PROJECT_ID" {
+  default = "salesrecon"
+}
+
+variable "DATA" {
+  default = "coca-cola"
+}
